09c9e8a840df7dc560b6a62e7f17a0e31f2f6d57
Xayze0 <66648148+Xayze0@users.noreply.github.com>
2023-05-09 12:35:20 -0400

    Update parameters.ps1

--- parameters.ps1
+++ parameters.ps1
@@ -1,25 +1,25 @@
 ﻿# Script Parameters for Revive-ToolBox.ps1
 <#
     Author             : Bryant Bennett
-    Last Edit          : BB - 07/07/2020
+    Last Edit          : BB - 05/09/2023
 #>
 
 @{
     #-- Revive Image.exe Command Args
-        RVImageCmdArg1 = "qp"
-        RVImageCmdArg3 = 'd=$n'
+        #RVImageCmdArg1 = "qp"
+        #RVImageCmdArg3 = 'd=$n'
         
-        RVTools = [ordered]@{ RTCollectBackupSizes = "Collect Backup Sizes"; RTSPXErrors = "Find SPX Errors in logs"; RTRemoveOldInc = "Move unrequired .SPI Files" ; RTVerifyChain = "Verify Chains"}
+        #RVTools = [ordered]@{ RTCollectBackupSizes = "Collect Backup Sizes"; RTSPXErrors = "Find SPX Errors in logs"; RTRemoveOldInc = "Move unrequired .SPI Files" ; RTVerifyChain = "Verify Chains"}
 
-        RVToolsIM = [ordered]@{RTIMUsageReport = "IM Usage Report (Under Construction)"}
+        #RVToolsIM = [ordered]@{RTIMUsageReport = "IM Usage Report (Under Construction)"}
 
-        Title = "Revive-ToolBox"
+        Title = "Eclipse Onboard ToolBox"
 
-        Version = "3.0.4"  
+        Version = "1.0.1"  
 
-        Exclusions = "Old Chains","spf.tmp","bitmap","OldChains","Incrementals"
+        #Exclusions = "Old Chains","spf.tmp","bitmap","OldChains","Incrementals"
            
-        IMLogQuerry = "Collapse Daily Exception: Monthly collapse aborted due to verification status of the following files: ReverifyQueued :"
+        #IMLogQuerry = "Collapse Daily Exception: Monthly collapse aborted due to verification status of the following files: ReverifyQueued :"
 
        
 }
